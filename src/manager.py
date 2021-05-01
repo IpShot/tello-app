a8bbda89d0bb89c1c83d85d8653b89cf54873e88
IpShot <zhernosek.roman@gmail.com>
2021-05-01 12:34:27 +0500

    add joystick disconnect/reconnect handle

--- src/manager.py
+++ src/manager.py
@@ -8,9 +8,6 @@ class Manager:
         self.controller = controller
         self.route = route
 
-        # Update drone state (send a command) every 20 milliseconds
-        pygame.time.set_timer(pygame.USEREVENT + 1, 20)
-
     def handle_event(self, e):
         if e.type == pygame.USEREVENT + 1:
             move_data = self.controller.update()
@@ -18,15 +15,28 @@ class Manager:
             self.controller.handle_event(e, self.route)
 
     def start(self):
-        joystick_connected = False
+        pygame.init()
+        # Update drone state (send a command) every 20 milliseconds
+        pygame.time.set_timer(pygame.USEREVENT + 1, 20)
+
         print ('Looking for joystick...')
+        joystick_connected = False
         while True:
-            if not joystick_connected and pygame.joystick.get_count() > 0:
-                ds4 = pygame.joystick.Joystick(0)
-                ds4.init()
-                print('Joystick connected: ' + ds4.get_name())
-                joystick_connected = True
+            if joystick_connected and pygame.joystick.get_count() == 0:
+                self.controller.stop()
+                joystick_connected = False
+                print('Joystick disconnected')
+                print ('Looking for joystick...')
+
+            if not joystick_connected:
+                pygame.joystick.quit()
+                pygame.joystick.init()
+                if pygame.joystick.get_count() > 0:
+                    ds4 = pygame.joystick.Joystick(0)
+                    ds4.init()
+                    joystick_connected = True
+                    print('Joystick connected: ' + ds4.get_name())
 
             if joystick_connected:
                 for e in pygame.event.get():
-                    manager.handle_event(e)
+                    self.handle_event(e)
