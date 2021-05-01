10ef256a39fb825600d45ae42d3f6dbd3d588275
IpShot <zhernosek.roman@gmail.com>
2021-05-01 10:42:23 +0500

    change tabs to spaces

--- src/main.py
+++ src/main.py
@@ -9,30 +9,30 @@ from route import Route
 from manager import Manager
 
 def main():
-	dev = False
-	drone = Tello()
+    dev = True
+    drone = Tello()
 
-	if not dev:
-		drone.connect()
-		drone.streamoff()
-		drone.streamon()
+    if not dev:
+        drone.connect()
+        drone.streamoff()
+        drone.streamon()
 
-	controller = Controller(drone, dev=dev)
-	route = Route(drone, controller)
-	manager = Manager(controller, route)
+    controller = Controller(drone, dev=dev)
+    route = Route(drone, controller)
+    manager = Manager(controller, route)
 
-	try:
-		manager.start()
-	except KeyboardInterrupt as e:
-		pass
-	except Exception as e:
-		exc_type, exc_value, exc_traceback = sys.exc_info()
-		traceback.print_exception(exc_type, exc_value, exc_traceback)
-		print(e)
+    try:
+        manager.start()
+    except KeyboardInterrupt as e:
+        pass
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+        print(e)
 
-	if not dev:
-		drone.end()
+    if not dev:
+        drone.end()
 
 
 if __name__ == '__main__':
-	main()
+    main()
